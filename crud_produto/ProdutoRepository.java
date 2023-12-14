b360c48e47fe4ff9689619eb12086f5d3c1c33bb
Vinicius Lima <vinicius.slima99@icloud.com>
2023-12-14 15:47:19 -0300

    Criando as classes de produto

---
+++ crud_produto/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package br.com.pixau.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import br.com.pixau.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
+}
