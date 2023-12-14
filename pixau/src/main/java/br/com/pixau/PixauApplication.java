a8212e21e65e231ebe9bffc028da3a0cd2b013a6
Vinicius Lima <148589151+vinysl@users.noreply.github.com>
2023-12-14 15:56:29 -0300

    Add files via upload

---
+++ pixau/src/main/java/br/com/pixau/PixauApplication.java
@@ -0,0 +1,13 @@
+package br.com.pixau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixauApplication.class, args);
+	}
+
+}
